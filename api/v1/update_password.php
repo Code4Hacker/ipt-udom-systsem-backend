83476db8e3013981d2bb9cb077b39c1396439101
Code4Hacker <black2022mail@gmail.com>
2024-03-26 08:59:57 +0300

    passcode

---
+++ api/v1/update_password.php
@@ -0,0 +1,39 @@
+<?php
+include_once("connector.php");
+
+function sanitize_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+switch($_SERVER['REQUEST_METHOD']){
+    case "POST":
+        $id = sanitize_input($_POST['studentId']);
+        $old_password = sanitize_input($_POST['old']);
+        $new_pwd = sanitize_input($_POST['new_pwd']);
+        if(!empty($id) && !empty($old_password)){
+            $sql_check = "SELECT * FROM STUDENTS WHERE t_number = '$id' AND password = '$old_password'";
+            $returned = $conn -> query($sql_check);
+
+            if($returned){
+                $update_sql = "UPDATE STUDENTS SET password='$new_pwd' WHERE t_number = '$id' AND password = '$old_password'";
+
+                $response_back = $conn -> query($update_sql);
+                if($response_back){
+                    echo json_encode(array("status" => 200, "message" => "update Success"));
+                }else{
+                    echo json_encode(array("status" => 500, "message" => "Error.".$conn -> error));
+                }
+            }else{
+                echo json_encode(array("status" => 500, "message" => "Error.".$conn -> error));
+            }
+        }else{
+            echo json_encode(array("status" => 402, "message" => "Error.".$conn -> error));
+        }
+        break;
+    default:
+        break;
+}
+?>
